709484078230704c744cae7b8b2d8154fcae3b83
uchenna Iwuchukwu <oreoluwa371@gmail.com>
2024-01-09 09:40:36 +0100

    creation of logout test case

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,9 +1,8 @@
+import { userName,passWord,url,facility,date,confirmationText } from "./param.cy";
 /// <reference types="cypress" />
-const userName = "John Doe";
-const passWord = "ThisIsNotAPassword";
-const url = 'https://katalon-demo-cura.herokuapp.com/'
+
 describe('E2E UI Test', () => {
-  it('LoginAndBookAppointment', () => {
+  it('BookAppointmentAndLogout', () => {
     cy.visit(url)
     //click make appointment 
     cy.get("#btn-make-appointment").click();
@@ -14,17 +13,28 @@ describe('E2E UI Test', () => {
     cy.get('#btn-login').click();
 
     //book Appointment 
-    cy.get('#combo_facility').select("Hongkong CURA Healthcare Center");
+    cy.get('#combo_facility').select(facility);
     cy.get('#radio_program_medicaid').click();
+    cy.get('#radio_program_medicaid').invoke('text').as('valueRadioProgram')
     cy.get('.checkbox-inline').click();
     cy.get('.col-sm-4 > :nth-child(2)').click();
-    cy.get('#txt_visit_date').type("16/01/2024");
+    cy.get('#txt_visit_date').type(date);
     cy.get('#btn-book-appointment').click();
+    
 
     //contains Appointment Confirmation and confirms readmission status being yes 
     cy.get('#summary');
-    cy.get('h2').contains("Appointment Confirmation");
+    cy.get('h2').contains(confirmationText);
     cy.get('#hospital_readmission').contains("Yes");
+    cy.get('#facility').contains(facility);
+    cy.get('#visit_date').contains(date);
+    cy.get('.text-center > .btn').click();
+    
+    //logout
+    cy.get('#menu-toggle').click();
+    cy.get(':nth-child(6) > a').click();
 
+    
   })
+
 })
---
+++ cypress/e2e/param.cy.js
@@ -0,0 +1,8 @@
+const userName = "John Doe";
+const passWord = "ThisIsNotAPassword";
+const url = 'https://katalon-demo-cura.herokuapp.com/'
+const facility = "Hongkong CURA Healthcare Center";
+const date = "16/01/2024";
+const confirmationText = "Appointment Confirmation";
+
+export {userName, passWord, url, facility, date, confirmationText}
